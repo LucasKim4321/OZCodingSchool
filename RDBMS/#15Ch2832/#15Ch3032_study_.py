46c7e7375af5ff5bd90c4d07fd651007767ffe2c
LucasKim4321 <xowls0131@naver.com>
2025-01-20 22:21:17 +0900

    study

--- RDBMS/#15Ch2832/#15Ch3032_study_.py
+++ RDBMS/#15Ch2832/#15Ch3032_study_.py
@@ -2,4 +2,16 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 ChromeDriverManager().install()
 
+# path 에러 생길 시
+# 1. 상대경로 설정
+# path = './chromedriver'
+
+#2. 절대경로 설정
+# import os
+# path = os.getcwd() + '/chromedriver'
+
 browser = webdriver.Chrome
+
+browser.back() # 뒤로가기
+browser.forward() # 앞으로가기
+browser.refresh() # 새로고침
